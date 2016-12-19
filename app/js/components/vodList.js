36210d42fe36e2358c4b7c9e7a9ad7fcf6b95680
AJ Danelz <vordimous@outlook.com>
2016-12-19 18:20:11 -0500

    little bit a tag searching swag

--- app/js/components/vodList.js
+++ app/js/components/vodList.js
@@ -1,31 +1,40 @@
 
 ko.components.register('vod-list', {
-    template: { fromUrl: 'components/vodList.html', postRender:function(){
-        
-        $('#vods').DataTable({
-            "paging": true,
-            "lengthChange": true,
-            "searching": true,
-            "ordering": true,
-            "autoWidth": false
-        });
-    }},
+    template: { fromUrl: 'components/vodList.html'},
     viewModel: function(params){
         var vm = {};
         vm.title = "Vods"
         vm.data = ko.observableArray([]);
-        v.service.vod.list({tagIds:[9,8]}).then(function(vods){
-            var data = [];
-            _.each(vods, function(vod){
-                data.push(v.model.vod.create(vod));
-            })
-            vm.data(data);
-        })
 
         vm.add = function(){
             vm.data.push(v.model.vod.create());
-        }
+        };
 
+        vm.selTags = ko.observableArray();
+        vm.tags = ko.observableArray([]);
+        v.service.tag.list().then(function(tags){
+            var data = [];
+            _.each(tags, function(tag){
+                data.push(v.model.tag.create(tag));
+            })
+            vm.tags(data);
+        })
+
+        var query = ko.computed(function(){
+            return {tagIds:vm.selTags()}
+        });
+        query.subscribe(function(obj){
+            v.service.vod.list(obj).then(function(vods){
+                var data = [];
+                _.each(vods, function(vod){
+                    data.push(v.model.vod.create(vod));
+                })
+                vm.data(data);
+            });
+        });
+
+        vm.selTags([9,8]);
+        
         return vm;
     }
 });
