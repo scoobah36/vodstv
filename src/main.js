b2735784f6a36694d349ce6d5351101c6492c903
scoobah36 <vordimous@outlook.com>
2017-02-22 07:35:19 -0500

    add an element theme

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
+import '../theme/index.css'
 import Element from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
 Vue.use(Element)
 
 // import components
